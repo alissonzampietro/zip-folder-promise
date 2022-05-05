05691cef2955fa22e2af999fd66b0441d76b283c
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2022-05-04 23:13:33 -0500

    refactor: use try/catch for better error handling

--- index.js
+++ index.js
@@ -3,39 +3,43 @@ const archiver = require('archiver');
 
 function zipFolderPromise(dirname, outputFile, format = 'zip', subDirectory = '') {
   return new Promise((resolve, reject) => {
-    let archiveOpts;
-    switch (format) {
-      case 'zip':
-        archiveOpts = {
-          zlib: { level: 9 },
-        };
-        break;
+    try {
+      let archiveOpts;
+      switch (format) {
+        case 'zip':
+          archiveOpts = {
+            zlib: { level: 9 },
+          };
+          break;
 
-      case 'tar':
-        archiveOpts = {
-          gzip: true,
-          gzipOptions: { level: 9 },
-        };
-        break;
+        case 'tar':
+          archiveOpts = {
+            gzip: true,
+            gzipOptions: { level: 9 },
+          };
+          break;
 
-      default:
-        reject(new Error("Error: Only 'zip' and 'tar' formats are supported."));
-    }
+        default:
+          throw new Error("Error: Only 'zip' and 'tar' formats are supported.");
+      }
 
-    const output = fs.createWriteStream(outputFile);
-    const archive = archiver(format, archiveOpts);
+      const output = fs.createWriteStream(outputFile);
+      const archive = archiver(format, archiveOpts);
 
-    output.on('close', () => {
-      resolve(`${archive.pointer()} bytes written`);
-    });
+      output.on('close', () => {
+        resolve(`${archive.pointer()} bytes written`);
+      });
 
-    archive.on('error', (err) => {
+      archive.on('error', (err) => {
+        throw err;
+      });
+
+      archive.pipe(output);
+      archive.directory(dirname, subDirectory || false);
+      archive.finalize();
+    } catch (err) {
       reject(err);
-    });
-
-    archive.pipe(output);
-    archive.directory(dirname, subDirectory || false);
-    archive.finalize();
+    }
   });
 }
 
